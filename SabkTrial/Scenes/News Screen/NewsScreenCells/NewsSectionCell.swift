f740f4a5e817f32f3de92202b5b2ca0b6b6c7dc6
samira273 <samiramarassy@gmail.com>
2019-10-21 12:23:33 +0200

    news section done

---
+++ SabkTrial/Scenes/News Screen/NewsScreenCells/NewsSectionCell.swift
@@ -0,0 +1,24 @@
+//
+//  NewsSectionCell.swift
+//  SabkTrial
+//
+//  Created by Samira.Marassy on 10/21/19.
+//  Copyright © 2019 Samira.Marassy. All rights reserved.
+//
+
+import UIKit
+
+class NewsSectionCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
